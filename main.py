c1d6d1a2433c57ecd952aac946789dd55ee09c31
Joseph Yan'dev <joedevelopermiester@gmail.com>
2017-11-11 17:46:01 -0500

    Removed sockets

--- main.py
+++ main.py
@@ -2,14 +2,15 @@
 import pygame, socket, math, argparse
 from src.gamedata import GameData
 
-def main ():
+def main (argsList):
     pygame.init()
     clock = pygame.time.Clock()
     data = GameData()
+    # Initialize Screen:
     screen = pygame.display.set_mode((data.window.width, data.window.height))
     running = True
     while running:
-        time = clock.tick(60) #similar to timerDelay
+        time = clock.tick(60) # similar to timerDelay
         screen.fill((255,0,0))
         for event in pygame.event.get():
             handle(event,data)
@@ -53,13 +54,5 @@ def handleMovement(event,data):
     if "right" in data.keysPressed:
         dir = (1,0)
 
-def handle(event):
-    if event.type == pygame.QUIT:
-        running = False
-
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser("Run Zauntlet with parameters.")
-    parser.add_argument("--hostIP", action="store", dest="hostIP",
-                        help="The host IP to connect to")
-    argsList = parser.parse_args() # Get list of arguments
     main()
